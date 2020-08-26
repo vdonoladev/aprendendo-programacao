98003a6bc785cf7aa2704c2b91b121b1b27e30a2
Víctor Donola Ferreira <victordonolaf@gmail.com>
2020-08-26 18:12:12 -0300

    javascript codes

---
+++ JavaScript/Curso_JavaScript&TypeScript/aula27(funcoes-basico)/index.js
@@ -0,0 +1,48 @@
+/* function saudacao(nome) {
+  // console.log(`Bom dia ${nome}!`);
+  return `Bom dia ${nome}!`;
+}
+
+const variavel = saudacao('Víctor');
+console.log(variavel); */
+// function soma(x , y)
+/* function soma(x = 1, y = 1) {
+  const resultado = x + y;
+  return resultado;
+  // console.log('Olá mundo!');
+} */
+
+// console.log(soma(2, 2));
+
+// console.log(soma(2, 2));
+// console.log(resultado); // ReferenceError: resultado is not defined
+
+// const resultado = soma(2, 2);
+// console.log(resultado);
+
+// const resultado = soma(5, 10);
+// const resultado = soma('Víctor', ' Donola'); // concatenou
+// const resultado = soma(4, 2);
+// console.log(resultado);
+
+/* const raizQ = function (n) {
+  return n ** 0.5;
+};
+
+console.log(raizQ(9));
+console.log(raizQ(16));
+console.log(raizQ(25)); */
+
+/* const raizQ = (n) => n ** 0.5;
+
+console.log(raizQ(9));
+console.log(raizQ(16));
+console.log(raizQ(25)); */
+
+const raizQ = (n) => {
+  return n ** 0.5;
+};
+
+console.log(raizQ(9));
+console.log(raizQ(16));
+console.log(raizQ(25));
