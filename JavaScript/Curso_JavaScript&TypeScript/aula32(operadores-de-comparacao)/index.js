62faa29ac892204b26249d9839c3495b1bce8a11
Víctor Donola Ferreira <victordonolaf@gmail.com>
2020-08-28 01:13:20 -0300

    javascript codes

---
+++ JavaScript/Curso_JavaScript&TypeScript/aula32(operadores-de-comparacao)/index.js
@@ -0,0 +1,27 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que 
+<= menor que ou igual a
+== igualdade (valor) (= atribuição) // não usar
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) // não usar
+!== diferente estrito (valor e tipo)
+ */
+// const num1 = 10;
+// const num2 = 5;
+// const comp = num1 > num2;
+const comp = 10 !== 10;
+console.log(comp);
+// console.log(10 > 5); // true
+// console.log(10 >= 5); // true
+// console.log(10 < 5); // false
+// console.log(10 <= 5); // false
+// console.log(10 == 5); // false
+// console.log(10 === 5); // false
+// console.log(10 === '10'); false
+// console.log(10 === 10); // true
+// console.log(10 != 5); // true
+// console.log(10 !== 5); // true
+// console.log(10 !== 10); // false
