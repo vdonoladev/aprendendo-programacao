757649cd92e828b5eccc457cbcb8b567c848c4f2
Víctor Donola Ferreira <victordonolaf@gmail.com>
2020-09-02 23:14:48 -0300

    javascript codes

--- JavaScript/Curso_JavaScript&TypeScript/aula36(if-else-if-e-else(2))/index.js
+++ JavaScript/Curso_JavaScript&TypeScript/aula36(if-else-if-e-else(2))/index.js
@@ -13,6 +13,16 @@ if (numero >= 0 && numero <= 5) {
 
 // console.log('O número NÃO está entre 0 e 5.'); // Independente. Vai rodar independente do if
 
+/*
+if (1 === 1) {
+	console.log('LITERAL'); // Verdadeiro
+}
+*/
+
+if (numero <= 10) {
+	console.log('O número é menor ou igual a 10.');
+}
+
 if (numero >= 0 && numero <= 5) {
 	console.log('O número está entre 0 e 5.');
 } else if (numero >= 6 && numero <= 8) {
@@ -22,3 +32,19 @@ if (numero >= 0 && numero <= 5) {
 } else {
 	console.log('O número não está entre 0 e 11.');
 }
+
+/*
+if (numero >= 0 && numero <= 5) {
+	console.log('O número está entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+	console.log('O número está entre 6 e 8.');
+} else if (1 === 1) {
+	console.log('LITERAL'); // Verdadeiro // Vai desestruturar todo o código
+} else if (numero >= 9 && numero <= 11) { // Verdadeiro
+	console.log('O número está entre 9 e 11.');
+} else {
+	console.log('O número não está entre 0 e 11.');
+}
+*/
+
+console.log('...Aqui vai o resto do código.'); // Independente. Vai rodar independente do if
