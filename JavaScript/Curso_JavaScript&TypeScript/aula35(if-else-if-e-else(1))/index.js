6f460035ff88de7a6eb19ce225a8ef2cce6c028d
Víctor Donola Ferreira <victordonolaf@gmail.com>
2020-09-02 22:42:04 -0300

    javascript codes

---
+++ JavaScript/Curso_JavaScript&TypeScript/aula35(if-else-if-e-else(1))/index.js
@@ -0,0 +1,35 @@
+/*
+Entre 0 - 11 - Bom dia
+Entre 12 - 17 - Boa tarde
+Entre 18 - 23 - Boa noite
+*/
+
+// if pode ser usado sozinho
+// else if não pode ser executado sozinho. Sempre que utilizo a palavra else, preciso de um if antes
+// Eu posso ter vários else if na checagem (quantos eu quiser)
+// Só posso ter um else na checagem
+// Podemos usar condições sem else if, utilizando apenas if e else
+
+/*
+const hora = 18;
+
+if (hora >= 0 && hora <= 11) {
+	console.log('Bom dia');
+} else if (hora >= 12 && hora <= 17) {
+	console.log('Boa tarde');
+} else if (hora >= 18 && hora <= 23) {
+	console.log('Boa noite');
+} else {
+	console.log('Formato de horário incorreto. Tente Novamente!');
+}
+*/
+
+/*
+const tenhoGrana = true;
+
+if (tenhoGrana) {
+	console.log('Vou sair de casa');
+} else {
+	console.log('Não vou sair de casa');
+}
+*/
