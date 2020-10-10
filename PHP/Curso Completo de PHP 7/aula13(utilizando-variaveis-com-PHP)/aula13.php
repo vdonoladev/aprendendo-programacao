daad8146a1083d54ecf800c826dc8bc689317669
vdonoladev <victordonolaf@gmail.com>
2020-10-10 17:41:18 -0300

    Códigos PHP

---
+++ PHP/Curso Completo de PHP 7/aula13(utilizando-variaveis-com-PHP)/aula13.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Seção 3 - Conhecendo a Linguagem PHP
+
+Aula 13 - Utilizando Variáveis com PHP */
+
+$nome = "Hcode"; // Variáveis começam com $
+
+// echo $nome;
+
+var_dump($nome); // Mostra o tipo de dado, o tamanho e o valor
+
+?>
