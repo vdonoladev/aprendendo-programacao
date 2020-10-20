d6a8cef933d44ed6a95de0f77d7bd62904b12bd7
vdonoladev <victordonolaf@gmail.com>
2020-10-20 18:33:54 -0300

    Codigos JavaScript

--- JavaScript/Curso_JavaScript&TypeScript/aula44(mais-diferencas-entre-var-e-let-const)/index.js
+++ JavaScript/Curso_JavaScript&TypeScript/aula44(mais-diferencas-entre-var-e-let-const)/index.js
@@ -3,10 +3,31 @@ const verdadeira = true;
 // let tem escopo de bloco {... bloco}
 // var só tem escopo de função
 
-let nome = 'Luiz';
-var nome2 = 'Luiz';
+/* let nome = 'Luiz'; // Criando
+var nome2 = 'Luiz'; // Criando
 
 if (verdadeira) {
-    let nome = 'Otávio';
-    console.log(nome, nome2);
+  let nome = 'Otávio'; // Criando
+  var nome2 = 'Rogério'; // Redeclarando
+
+  if (verdadeira) {
+    var nome2 = 'Ronaldo'; // Redeclarando
+    let nome = 'Outra coisa';
+  }
 }
+
+console.log(nome, nome2); */
+
+/* function falaOi() {
+  if (verdadeira) {
+    let nome = 'Luiz';
+    var sobrenome = 'Miranda';
+  }
+  console.log(sobrenome);
+}
+
+falaOi(); */
+
+console.log(sobrenome);
+
+var sobrenome = 'Miranda';
