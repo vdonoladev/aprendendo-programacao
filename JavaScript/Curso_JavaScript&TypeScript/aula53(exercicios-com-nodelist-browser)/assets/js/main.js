03994ac46a98fe5507ca7855cd5197ef0e62968f
vdonoladev <victordonolaf@gmail.com>
2020-10-23 14:56:31 -0300

    JavaScript codes

---
+++ JavaScript/Curso_JavaScript&TypeScript/aula53(exercicios-com-nodelist-browser)/assets/js/main.js
@@ -0,0 +1,12 @@
+const paragrafos = document.querySelector('.paragrafos');
+
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+
+
+for (let p of ps) {
+	p.style.backgroundColor = backgroundColorBody;
+	p.style.color = "#ffffff";
+}
