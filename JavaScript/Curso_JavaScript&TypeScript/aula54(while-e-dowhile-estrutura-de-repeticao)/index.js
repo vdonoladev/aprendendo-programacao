b033135c94b7d9458680867465bed3e49dd1b625
vdonoladev <victordonolaf@gmail.com>
2020-10-23 16:45:50 -0300

    JavaScript codes

---
+++ JavaScript/Curso_JavaScript&TypeScript/aula54(while-e-dowhile-estrutura-de-repeticao)/index.js
@@ -0,0 +1,36 @@
+/* function random(min, max) {
+	const r = Math.random() * (max - min) + min;
+	return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+
+let rand = random(min, max);
+
+console.log(rand !== 10);
+
+while (rand !== 10) {
+	rand = random(min, max);
+	console.log(rand);
+} */
+
+function random(min, max) {
+	const r = Math.random() * (max - min) + min;
+	return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+
+let rand = 10;
+
+while (rand !== 10) {
+	rand = random(min, max);
+	console.log(rand);
+} 
+
+do {
+	rand = random(min, max);
+	console.log(rand);
+} while (rand !== 10);
