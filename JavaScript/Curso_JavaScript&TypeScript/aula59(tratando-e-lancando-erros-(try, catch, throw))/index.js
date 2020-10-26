78f4ec42ab4abd2fafd145534d964ee149b64da0
vdonoladev <victordonolaf@gmail.com>
2020-10-26 16:39:33 -0300

    JavaScript codes

---
+++ JavaScript/Curso_JavaScript&TypeScript/aula59(tratando-e-lancando-erros-(try, catch, throw))/index.js
@@ -0,0 +1,15 @@
+function soma(x, y) {
+	if (typeof x !== 'number' || typeof y !== 'number') {
+		throw new Error('x e y precisam ser números.');
+	}
+
+	return x + y;
+}
+
+try {
+	console.log(soma(1, 2));
+	console.log(soma(1, '2'));
+} catch (error) {
+	// console.log(error);
+	console.log('Alguma coisa mais amigável para o usuário.')
+}
