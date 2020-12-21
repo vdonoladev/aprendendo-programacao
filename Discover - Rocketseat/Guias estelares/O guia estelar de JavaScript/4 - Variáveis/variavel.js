c7c85e6b9a59eabb215a281f27f00c57f5ae59c2
vdonoladev <victordonolaf@gmail.com>
2020-12-21 04:33:36 -0300

    Cursos da Rocketseat

---
+++ Discover - Rocketseat/Guias estelares/O guia estelar de JavaScript/4 - Variáveis/variavel.js
@@ -0,0 +1,26 @@
+// var
+var clima = "Quente"
+
+console.log(clima) // Quente
+
+clima = "Frio"
+
+console.log(clima) // Frio
+
+// let
+let clima = "Quente"
+
+console.log(clima) // Quente
+
+clima = "Frio"
+
+console.log(clima) // Frio
+
+// const (constante = imutável)
+const clima = "Quente"
+
+console.log(clima) // Quente
+
+clima = "Frio"
+
+console.log(clima) // TypeError
