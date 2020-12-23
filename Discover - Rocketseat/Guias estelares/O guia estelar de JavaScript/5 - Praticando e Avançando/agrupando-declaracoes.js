adb704c11f88d5540b85fbc6df0b2e169f69ba1c
vdonoladev <victordonolaf@gmail.com>
2020-12-23 03:22:27 -0300

    Cursos da Rocketseat

---
+++ Discover - Rocketseat/Guias estelares/O guia estelar de JavaScript/5 - Praticando e Avançando/agrupando-declaracoes.js
@@ -0,0 +1,23 @@
+// Variáveis e tipos de dados
+
+// declaração or declaration
+var name
+
+// assignment or atribuição de valores
+name = "Mayk"
+
+// que tipo de dado foi colocado na variável
+console.log(typeof name)
+
+// ------------------------------------------
+
+let age = 20 // number
+let isHuman = true // boolean
+
+// agrupamento de declarações
+let age, isHuman
+
+age = 20
+isHuman = true
+
+console.log(name, age, isHuman)
---
+++ Discover - Rocketseat/Guias estelares/O guia estelar de JavaScript/5 - Praticando e Avançando/arrays.js
@@ -0,0 +1,10 @@
+// Array
+
+const animals = [
+	"Lion",
+	"Monkey",
+	"Cat"
+]
+
+// acessar valores dentro do array
+console.log(animals[3])
