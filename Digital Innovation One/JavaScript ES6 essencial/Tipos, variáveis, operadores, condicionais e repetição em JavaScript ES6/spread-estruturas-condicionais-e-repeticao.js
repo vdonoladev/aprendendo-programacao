edbde1572cbc11e4d20157e4c36cbb01364b030f
vdonoladev <victordonolaf@gmail.com>
2020-12-26 03:38:54 -0300

    Enviando novos códigos

--- Digital Innovation One/JavaScript ES6 essencial/Tipos, variáveis, operadores, condicionais e repetição em JavaScript ES6/spread-estruturas-condicionais-e-repeticao.js
+++ Digital Innovation One/JavaScript ES6 essencial/Tipos, variáveis, operadores, condicionais e repetição em JavaScript ES6/spread-estruturas-condicionais-e-repeticao.js
@@ -147,6 +147,37 @@ array.forEach(item => {
 
 // ------------------------------------------------------------------
 
+Switch
 
+switch (expressao){
+    case valor1:
+        [break;]
+    case valor2:
+        [break;]
+    default:
+        [break;]
+}
+
+const fruit = 'banana';
+
+switch(fruit){
+    case 'banana':
+        console.log('R$3,00 / kg');
+        break;
+    case 'mamão':
+    case 'pera':
+        console.log('R$2,00 / kg');
+        break;  
+    default:
+        console.log('Produto não existe no estoque');
+        break;     
+}
+
+
+-- OUTPUT --
+
+R$3,00 / kg
+
+// -------------------------------------------------------------------------------------------------
 
 */
