87fbc367f39dc48659e7000f3764b44c80af894d
vdonoladev <victordonolaf@gmail.com>
2021-01-02 05:58:28 -0300

    Enviando novos códigos

---
+++ PHP/Curso Completo de PHP 7/aula19(incluindo-arquivos-com-Include-e-Require)/aula19(function-somar).php
@@ -0,0 +1,15 @@
+<?php
+
+// include 'aula19.php';
+
+require 'aula19.php';
+
+// Require = obriga que o arquivo exista (o que você está incluindo) e que o arquivo esteja funcionando corretamente
+
+// Include = tenta funcionar (mesmo que o arquivo não exista) 
+
+$resultado = somar(10, 20);
+
+echo $resultado; // 30
+
+?>
---
+++ PHP/Curso Completo de PHP 7/aula19(incluindo-arquivos-com-Include-e-Require)/aula19.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Seção 3 - Conhecendo a Linguagem PHP
+Aula 19 - Incluindo arquivos com Include e Require */
+
+var_dump("Ok");
+
+function somar($a, $b){
+	return $a + $b;
+}
+
+?>
