5afa4b9b69d385f1386ce63f64fd87c7cb3dacde
vdonoladev <victordonolaf@gmail.com>
2021-02-19 14:44:59 -0300

    Enviando novos códigos

---
+++ JavaScript/Curso_JavaScript&TypeScript/Seção 4 - JavaScript - Funções - Avançado/Praticando com factory functions/assets/js/main.js
@@ -0,0 +1,65 @@
+function criaCalculadora() {
+	return {
+		display: document.querySelector('.display'),
+		btnClear: document.querySelector('.btn-clear'),
+
+		inicia() {
+			this.cliqueBotoes();
+		},
+
+		realizaConta() {
+			let conta = this.display.value;
+
+			try {
+				conta = eval(conta);
+
+				if(!conta) {
+					alert('Conta inválida!');
+					return;
+				}
+
+				this.display.value = String(conta);
+			} catch(e) {
+				alert('Conta inválida!');
+				return;
+			}
+		},
+
+		clearDisplay() {
+			this.display.value = '';
+		},
+
+		deleteUm() {
+			this.display.value = this.display.value.slice(0, -1);
+		},
+
+		cliqueBotoes() {
+			document.addEventListener('click', e => {
+				const el = e.target;
+
+				if(el.classList.contains('btn-num')) {
+					this.btnParaDisplay(el.innerText);
+				}
+
+				if(el.classList.contains('btn-clear')) {
+					this.clearDisplay();
+				}
+
+				if(el.classList.contains('btn-del')) {
+					this.deleteUm();
+				}
+
+				if(el.classList.contains('btn-eq')) {
+					this.realizaConta();
+				}
+			});
+		},
+
+		btnParaDisplay(valor) {
+			this.display.value += valor;
+		}
+	};
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
