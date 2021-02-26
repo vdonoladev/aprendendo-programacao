2ee5027d37ba589da02c9198a8e33ccea6acf1a7
vdonoladev <victordonolaf@gmail.com>
2021-02-26 04:18:02 -0300

    Enviando novos códigos

--- JavaScript/Curso_JavaScript&TypeScript/Seção 4 - JavaScript - Funções - Avançado/Funções geradoras/main.js
+++ JavaScript/Curso_JavaScript&TypeScript/Seção 4 - JavaScript - Funções - Avançado/Funções geradoras/main.js
@@ -36,23 +36,23 @@ function* geradora2() {
 // console.log(g2.next().value); // 1
 // console.log(g2.next().value); // 2
 
-function* geradora3() {
-	yield 0;
-	yield 1;
-	yield 2;
-}
+// function* geradora3() {
+// 	yield 0;
+// 	yield 1;
+// 	yield 2;
+// }
 
-function* geradora4() {
-	yield* geradora3();
-	yield 3;
-	yield 4;
-	yield 5;
-}
+// function* geradora4() {
+// 	yield* geradora3();
+// 	yield 3;
+// 	yield 4;
+// 	yield 5;
+// }
 
-const g4 = geradora4();
-for(let valor of g4) {
-	console.log(valor); // 0 1 2 3 4 5
-}
+// const g4 = geradora4();
+// for(let valor of g4) {
+// 	console.log(valor); // 0 1 2 3 4 5
+// }
 
 // A "concatenação" das funções geradoras g3 e g4
 
