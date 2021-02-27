fedc29e527298344f6eb680e3c269487db19475a
vdonoladev <victordonolaf@gmail.com>
2021-02-27 04:56:33 -0300

    Enviando novos códigos

---
+++ Discover - Rocketseat/Guias estelares/O guia estelar de JavaScript/8 - Expressões e Operadores/operadoresAtribuicao.js
@@ -0,0 +1,25 @@
+// Operadores de atribuição (Assignment)
+
+let x;
+
+console.log(x);
+
+// assignment
+x = 1;
+console.log(x);
+
+// addition assignment
+x += 2;
+console.log(x);
+
+// subtraction assignment
+x -= 1;
+console.log(x);
+
+// multiplication assignment
+x *= 2;
+console.log(x);
+
+// division assingment
+x /= 2;
+console.log(x);
