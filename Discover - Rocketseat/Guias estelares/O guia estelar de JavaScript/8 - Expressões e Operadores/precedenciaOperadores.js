789ff471f77d09350c1a9f22d76d354a011f7ea6
vdonoladev <victordonolaf@gmail.com>
2021-02-27 05:40:50 -0300

    Enviando novos códigos

---
+++ Discover - Rocketseat/Guias estelares/O guia estelar de JavaScript/8 - Expressões e Operadores/precedenciaOperadores.js
@@ -0,0 +1,17 @@
+/*
+	Operator precedence
+		Precedência de operadores
+	
+- grouping                   ()
+- negação e incremento       ! ++ --
+- multiplicação e divisão    * /
+- adição e subtração         + -
+- relacional                 < <= > >=c
+- AND                        == != === !==
+- OR                         &&
+- condicional                ?:
+- assignment                 = += -= *=
+
+*/
+
+console.log((2 + 5) * 10);
