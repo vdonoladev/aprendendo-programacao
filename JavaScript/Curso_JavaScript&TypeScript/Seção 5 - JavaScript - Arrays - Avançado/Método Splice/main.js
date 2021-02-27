eabf9022b9a1ac153b0486614182f1a1419d10af
vdonoladev <victordonolaf@gmail.com>
2021-02-27 01:42:08 -0300

    Enviando novos códigos

---
+++ JavaScript/Curso_JavaScript&TypeScript/Seção 5 - JavaScript - Arrays - Avançado/Método Splice/main.js
@@ -0,0 +1,14 @@
+//                0        1        2          3         4
+//               -5       -4       -3         -2        -1
+const nomes = ["Maria", "João", "Eduardo", "Gabriel", "Júlia"];
+// nomes.splice(índice, deletar, adicionar);
+// const removidos = nomes.splice(-2, Number.MAX_VALUE); // Remover de um lugar até o fim do Array
+// console.log(Number.MAX_VALUE); // Maior número que pode existir no JavaScript
+// const removidos = nomes.splice(-2, 1);
+// const removidos = nomes.splice(3, 1, "Víctor"); // Remove o Gabriel e adiciona o Víctor no lugar
+// const removidos = nomes.splice(-1, 1); // Simulando o .pop()
+// const removidos = nomes.splice(0, 1); // Simulando o .shift()
+// console.log(removidos);
+// nomes.splice(nomes.length, 0, "Víctor"); // Simulando o .push()
+// nomes.splice(0, 0, "Víctor", "Donola"); // Simulando o .unshift()
+console.log(nomes);
