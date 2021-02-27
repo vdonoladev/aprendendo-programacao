bc2d34002ba59573e514d13a952561602720c607
vdonoladev <victordonolaf@gmail.com>
2021-02-27 04:17:33 -0300

    Enviando novos códigos

---
+++ Discover - Rocketseat/Guias estelares/O guia estelar de JavaScript/8 - Expressões e Operadores/new.js
@@ -0,0 +1,13 @@
+/*
+
+	new
+
+	- left-hand-side expression
+	- criar um novo objeto
+
+*/
+
+let name = new String("Víctor");
+name.surName = "Donola";
+let age = new Number(22);
+console.log(name.surName, age);
