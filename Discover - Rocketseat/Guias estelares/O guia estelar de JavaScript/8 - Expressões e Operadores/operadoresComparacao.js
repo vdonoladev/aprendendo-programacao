7d79c6a1b091668cce9a9eb6fa2b26fd3a532894
vdonoladev <victordonolaf@gmail.com>
2021-02-27 04:47:45 -0300

    Enviando novos códigos

---
+++ Discover - Rocketseat/Guias estelares/O guia estelar de JavaScript/8 - Expressões e Operadores/operadoresComparacao.js
@@ -0,0 +1,30 @@
+// Operadores de comparação
+
+// Irá comparar valores e retornar um Boolean como resposta à comparação
+
+let one = 1;
+let two = 2;
+
+// ==     Igual
+console.log(one == two);
+
+// !=     Diferente
+console.log(one != two);
+
+// ===    Estritamente igual
+console.log(one === two);
+
+// !==    Estritamente diferente
+console.log(one !== two);
+
+// >      Maior
+console.log(one > two);
+
+// >=     Maior ou igual
+console.log(one >= two);
+
+// <      Menor
+console.log(one < two);
+
+// <=     Menor ou igual
+console.log(one <= two);
