2373f6405e6d84e3aa4571635a95ea8d7135dd43
vdonoladev <victordonolaf@gmail.com>
2021-03-09 16:57:24 -0300

    Enviando novos códigos

--- Digital Innovation One/Introdução à biblioteca JQuery/Aprenda o que é jQuery e Ajax/Prática (Códigos)/js/main.js
+++ Digital Innovation One/Introdução à biblioteca JQuery/Aprenda o que é jQuery e Ajax/Prática (Códigos)/js/main.js
@@ -1,4 +1,5 @@
 function consultaCep() {
+	$(".barra-progresso").show();
 	let cep = document.getElementById("cep").value;
 	let url = "https://viacep.com.br/ws/" + cep + "/json/";
 	$.ajax({
@@ -11,6 +12,16 @@ function consultaCep() {
 			$("#localidade").html(response.localidade);
 			$("#uf").html(response.uf);
 			$("#titulo_cep").html("CEP " + response.cep);
+			$(".cep").show();
+			$(".barra-progresso").hide();
 		}
 	})
 }
+
+$(function() {
+	$(".cep").hide();
+	$(".barra-progresso").hide();
+});
+
+
+
