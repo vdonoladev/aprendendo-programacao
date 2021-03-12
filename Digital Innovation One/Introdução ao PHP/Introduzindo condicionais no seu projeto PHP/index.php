67508f59e8a63a3f9222007ed936b36703af58cb
vdonoladev <victordonolaf@gmail.com>
2021-03-12 07:15:10 -0300

    Enviando novos códigos

--- Digital Innovation One/Introdução ao PHP/Introduzindo condicionais no seu projeto PHP/index.php
+++ Digital Innovation One/Introdução ao PHP/Introduzindo condicionais no seu projeto PHP/index.php
@@ -17,19 +17,31 @@ $categorias[] = 'Adulto';
 $categorias[] = 'Idoso';
 
 $nome = 'Eduardo';
-$idade = 25;
+$idade = 17;
 
 if($idade >= 6 && $idade <= 12) 
 {
-	echo "Infantil";
+	for($i = 0; $i <=count($categorias); $i++)
+	{
+		if($categorias[$i] == 'Infantil')
+			echo "O nadador ".$nome. " compete na categoria ".$categorias[$i];
+	}
 }
 else if ($idade >= 13 && $idade <= 18)
 {
-	echo "Adolescente";
+	for($i = 0; $i <=count($categorias); $i++)
+	{
+		if($categorias[$i] == 'Adolescente')
+			echo "O nadador ".$nome. " compete na categoria ".$categorias[$i];
+	}
 }
 else
 {
-	echo "Adulto";
+	for($i = 0; $i <=count($categorias); $i++)
+	{
+		if($categorias[$i] == 'Adulto')
+			echo "O nadador ".$nome. " compete na categoria ".$categorias[$i];
+	}
 }
 
 ?>
