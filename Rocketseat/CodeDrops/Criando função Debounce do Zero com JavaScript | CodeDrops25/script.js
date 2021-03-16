dd66e2abaa64d8cfba377241fe6a0f7f2c905ba1
vdonoladev <victordonolaf@gmail.com>
2021-03-16 15:37:54 -0300

    Enviando novos códigos

--- Rocketseat/CodeDrops/Criando função Debounce do Zero com JavaScript | CodeDrops25/script.js
+++ Rocketseat/CodeDrops/Criando função Debounce do Zero com JavaScript | CodeDrops25/script.js
@@ -1,16 +1,16 @@
 const filterUsers = async (name) =>
-    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${name}`)
-    .then(res => res.json())
+  fetch(`https://jsonplaceholder.typicode.com/users?name_like=${name}`)
+  .then(res => res.json())
 
 
-const debounceEvent = (fn, wait = 1000, time) =>  (...args) =>
-        clearTimeout(time, time = setTimeout(() => fn(...args), wait))
-        
+const debounceEvent = (fn, wait = 1000, time) => (...args) =>
+  clearTimeout(time, time = setTimeout(() => fn(...args), wait))
+
 
 function handleKeyUp(event) {
-    filterUsers(event.target.value)
-    .then(users => console.log(users.map( user => user.name )))
+  filterUsers(event.target.value)
+    .then(users => console.log(users.map(user => user.name)))
 }
 
 document.querySelector("input")
-.addEventListener("keyup", debounceEvent(handleKeyUp, 500))
+  .addEventListener("keyup", debounceEvent(handleKeyUp, 500))
