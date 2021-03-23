168904168647d7a502ac96b726020a2124e23391
vdonoladev <victordonolaf@gmail.com>
2021-03-23 17:52:10 -0300

    Enviando novos códigos

--- JavaScript/Semana JSExpert3/hacker-chat-aula01/client/src/cliConfig.js
+++
@@ -1,5 +0,0 @@
-export default class CliConfig {
-	static parseArguments(commands) {
-		
-	}
-}
--- JavaScript/Semana JSExpert3/hacker-chat/client/index.js
+++ JavaScript/Semana JSExpert3/hacker-chat/client/index.js
@@ -1,19 +1,23 @@
 /*
 node index.js \
-	--username vdonoladev \
-	--room sala01 \
-	--hostUri localhost
+    --username vdonoladev \
+    --room sala01 \
+    --hostUri localhost
 */
 
 
 import Events from 'events'
-import CliConfig from '.src/cliConfig.js';
+import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socker.js';
 import TerminalController from "./src/terminalController.js";
+
 
 const [nodePath, filePath, ...commands] = process.argv
 const config = CliConfig.parseArguments(commands)
-console.log('config', config)
+
 const componentEmitter = new Events()
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
 
 // const controller = new TerminalController()
 // await controller.initializeTable(componentEmitter)
