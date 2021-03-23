85cfeb3d85e5aef228ab84e1d54a3d416fe112da
vdonoladev <victordonolaf@gmail.com>
2021-03-23 17:24:25 -0300

    Enviando novos códigos

--- JavaScript/Curso_JavaScript&TypeScript/Seção 6 - JavaScript - Objetos e Prototypes - Avançado/Revisando Objetos/main.js
+++ JavaScript/Curso_JavaScript&TypeScript/Seção 6 - JavaScript - Objetos e Prototypes - Avançado/Revisando Objetos/main.js
@@ -26,3 +26,44 @@ pessoa1.sobrenome = 'Donola Ferreira';
 
 console.log(pessoa1.nome);
 console.log(pessoa1.sobrenome);
+
+// delete pessoa1.nome; // Apaga o nome
+
+pessoa1.idade = 23;
+pessoa1.falarNome = function() {
+	return (`${this.nome} está falando seu nome.`)
+};
+
+// pessoa1.falarNome();
+
+pessoa1.getDataNascimento = function() {
+	const dataAtual = new Date();
+	return dataAtual.getFullYear() - this.idade;
+};
+
+console.log(pessoa1.getDataNascimento());
+
+for (let chave in pessoa1) {
+	// console.log(chave);
+	console.log(pessoa1[chave]);
+}
+
+// ---------------------------------------------------------
+
+// Factory functions / Constructor functions / Classes
+
+// Factory functions
+function criaPessoa(nome, sobrenome) {
+	return {
+		nome,
+		sobrenome,
+		get nomeCompleto() {
+			return `${this.nome} ${this.sobrenome}`;
+		}
+	};
+}
+
+const p1 = criaPessoa('Víctor', 'Donola Ferreira');
+console.log(p1.nomeCompleto);
+
+// Constructor functions
