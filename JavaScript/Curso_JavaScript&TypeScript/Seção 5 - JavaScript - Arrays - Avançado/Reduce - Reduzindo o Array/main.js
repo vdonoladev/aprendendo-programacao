774b510dc79878a47138bf5ab392d49a5a6078f2
vdonoladev <victordonolaf@gmail.com>
2021-03-23 05:11:33 -0300

    Enviando novos códigos

--- JavaScript/Curso_JavaScript&TypeScript/Seção 5 - JavaScript - Arrays - Avançado/Reduce - Reduzindo o Array/main.js
+++ JavaScript/Curso_JavaScript&TypeScript/Seção 5 - JavaScript - Arrays - Avançado/Reduce - Reduzindo o Array/main.js
@@ -6,5 +6,5 @@ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 const total = numeros.reduce(function(acumulador, valor, indice, array) {
 	acumulador += valor;
 	return acumulador;
-}, 0);
+}, []);
 console.log(total);
