b4cb949ae95a08f3b39d350e6f85f16b059fb271
vdonoladev <victordonolaf@gmail.com>
2021-03-23 05:33:03 -0300

    Enviando novos códigos

--- JavaScript/Curso_JavaScript&TypeScript/Seção 5 - JavaScript - Arrays - Avançado/Reduce - Reduzindo o Array/main.js
+++ JavaScript/Curso_JavaScript&TypeScript/Seção 5 - JavaScript - Arrays - Avançado/Reduce - Reduzindo o Array/main.js
@@ -1,10 +1,13 @@
-// 1 - Some todos os números (Reduce)
-// 2 - Retorne um array com os pares (Filter)
-// 3 - Retorna um array com o dobro dos valores (Map)
-
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-const total = numeros.reduce(function(acumulador, valor, indice, array) {
-	acumulador += valor;
-	return acumulador;
-}, []);
-console.log(total);
+const pessoas = [
+	{ nome: "Luiz", idade: 62 },
+	{ nome: "Maria", idade: 23 },
+	{ nome: "Eduardo", idade: 55 },
+	{ nome: "Letícia", idade: 10 },
+	{ nome: "Rosana", idade: 64 },
+	{ nome: "Wallace", idade: 63 },
+];
+const maisVelha = pessoas.reduce(function(acumulador, valor) {
+	if(acumulador.idade > valor.idade) return acumulador;
+	return valor;
+});
+console.log(maisVelha);
