6f6dff709345e1c73e3f641268daf59e4a9f8f60
vdonoladev <victordonolaf@gmail.com>
2021-03-26 03:05:36 -0300

    Enviando novos códigos

--- JavaScript/Curso_JavaScript&TypeScript/Seção 6 - JavaScript - Objetos e Prototypes - Avançado/Object.defineProperty() e Object.defineProperties()/main.js
+++ JavaScript/Curso_JavaScript&TypeScript/Seção 6 - JavaScript - Objetos e Prototypes - Avançado/Object.defineProperty() e Object.defineProperties()/main.js
@@ -9,7 +9,26 @@ function Produto(nome, preco, estoque) {
 		writable: false, // pode alterar
 		configurable: true // configurável
 	});
+
+	Object.defineProperties(this, {
+		nome: {
+			enumerable: true, // mostra a chave
+			value: estoque, // valor
+			writable: false, // pode alterar
+			configurable: true // configurável
+		},
+		preco: {
+			enumerable: true, // mostra a chave
+			value: estoque, // valor
+			writable: false, // pode alterar
+			configurable: true // configurável
+		},
+	});
 }
 
 const p1 = new Produto('Camiseta', 20, 3);
 console.log(p1);
+
+for(let chave in p1) {
+	console.log(chave);
+}
