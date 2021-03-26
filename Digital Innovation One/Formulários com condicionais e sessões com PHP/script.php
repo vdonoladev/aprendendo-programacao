2df0c5f18b0008481529ade8a022aef970e6eb7f
vdonoladev <victordonolaf@gmail.com>
2021-03-26 03:36:07 -0300

    Implementando técnicas de validação de dados

--- Digital Innovation One/Formulários com condicionais e sessões com PHP/script.php
+++ Digital Innovation One/Formulários com condicionais e sessões com PHP/script.php
@@ -9,6 +9,21 @@ $categorias[] = 'Idoso';
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+if(empty($nome)) {
+	echo 'O nome não pode ser vazio';
+	return;
+}
+
+if(strlen($nome) < 3) {
+	echo 'O nome deve conter mais de 3 caracteres';
+	return;
+}
+
+if(strlen($nome) > 40) {
+	echo 'O nome é muito extenso';
+	return;
+}
+
 if ($idade >= 6 && $idade <= 12) {
 	for ($i = 0; $i <= count($categorias); $i++) {
 		if ($categorias[$i] == 'Infantil')
