9eb16169319b19be1988e06601ff4c46787b48c1
vdonoladev <victordonolaf@gmail.com>
2025-11-19 15:26:42 -0300

    comentários

---
+++ PHP/PHP 8: Curso de Desenvolvimento Web Completo/Seção 2: Básico I - Fundamentos do PHP 8/04 - Comentários/index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Olá, mundo!<br>";
+
+// <br> pula uma linha no navegador
+
+// Este é um comentário de linha única
+
+/* Este é um comentário
+   de múltiplas linhas */
+
+echo "Olá, mundo!<br>";
+
+/*
+   Comentários são importantes para documentar o código,
+   explicar a lógica e facilitar a manutenção futura.
+*/
+
+# Este é outro comentário de linha única
